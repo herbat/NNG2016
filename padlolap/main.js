7121f96efb8eedf6e317f4413323b377ab4550b3
Attila Herbert <herbertatus@gmail.com>
2016-11-01 20:56:21 +0100

    Small corrections

--- padlolap/main.js
+++ padlolap/main.js
@@ -7,31 +7,46 @@ var fs       = require('fs'),
     finname  = '/Users/atus/Documents/NNG/padlolap/num.txt',
     foutname = '/Users/atus/Documents/NNG/padlolap/out.txt';
 
-function half() {
+function half(n) {
 
-    var rS = fs.createReadStream (finname, {highWaterMark:1}),//open file stream
-        wS = fs.createWriteStream(foutname),//open outfile stream
+    var rS     = fs.createReadStream(finname, {highWaterMark:1}),//open file stream
+        wS     = fs.createWriteStream(foutname),//open outfile stream
         stream = through(write, end),//create transform stream
-        r = 0;//remainder
+        r      = 0,//remainder
+        count  = 0;//counter to do exactly the desired amount of digits
 
     rS.on('open', () => {rS.pipe(stream); stream.pipe(wS);});
+
     //pipe out through transform stream
 
     function write(buffer, enc, next) {
+        function isWhole(n) {return n%1===0;}
+
         var res;
         buffer = parseInt(buffer.toString());//buf to int
+
         //division by 2
         if (isWhole(buffer/2)) {res = buffer/2     + r; r = 0;}
         else                   {res = buffer/2-0.5 + r; r = 5;}
         this.push(res.toString());//push to outstream
+
+        //close on too many digits
+        count++;
+        if(count > n) rS.close();
         next();
 
-        function isWhole(n) {return n%1===0;}
     }
 
-    function end(done){done();}
+    function padZeroes(n){
+        for(var i = 0; i < n-1; i++){
+            wS.write(0);
+        }
+        wS.write(1);
+    }
+
+    function end(done){padZeroes(n); done();}
 }
 
-half();
+half(9);
 
 
